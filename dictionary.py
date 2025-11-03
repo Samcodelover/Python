d3104c8d4656ae8bf976baa6583ca857ce7512df
Samcodelover <samal010590@gmail.com>
2025-11-02 20:43:03 -0500

    hw week6 dictionary

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+print("Enter a text: ")
+text = input()
+text = text.lower()
+dict = {}
+words = text.split()
+
+
+for word in words:
+    if word in dict:
+        dict[word] += 1
+    else:
+        dict[word] = 1
+print(dict)
+
+
+
